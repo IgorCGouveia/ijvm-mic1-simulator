e3d63a8a4675b1744976b5827296ba8c0485da73
Seu Nome <cgouveiaigor@gmail.com>
2025-09-06 21:07:02 -0300

    feat: Adiciona dois bits na instrução

--- ula.cpp
+++ ula.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-pair<string, int> execULA(int opcode, int a, int b, int inv) {
+pair<string, int> execULA(int opcode, int a, int b, int inv, int sll8, int sra1) {
     int result = 0;
     int iout = 0;
     switch (opcode) {
@@ -25,13 +25,18 @@ pair<string, int> execULA(int opcode, int a, int b, int inv) {
         case 49: result = 1; break;
         default: result = -1; break;
     }
+
+    if (sll8) result = result<<1;
+    
+    if (sra1) result = result>>1;
+
     if(result > 1) iout = 1;
     return {bitset<1>(result).to_string(), iout};
 }
 
 int main(int argc, char* argv[]) {
   int A, B, INV; cin >> A >> B >> INV;
-  int PC = 1;
+  int PC = 1; int SLL8; int SRA1;
 
   ifstream input ("./in.txt");
   if(!input){
@@ -47,8 +52,10 @@ int main(int argc, char* argv[]) {
 
   string l;
   while (getline(input, l)) {
-    cout << "OP:" << btod(l) << endl;
-    pair<string, int> result = execULA(btod(l), A, B, INV);
+    SLL8 = l[0] - '0';
+    SRA1 = l[1] - '0';
+    cout << "OP:" << btod(l.substr(2)) << endl;
+    pair<string, int> result = execULA(btod(l.substr(2)), A, B, INV, SLL8, SRA1);
     cout << result.first << ", " << result.second << endl;
 
     output << "PC=" << PC
